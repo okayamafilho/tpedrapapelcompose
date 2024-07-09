516f66a95342d6d1eb0c200cb54607b6d9875b3a
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2024-07-09 12:40:05 -0300

    terceiro commit - projeto papel e tesoura com compose - atualizando layout

--- app/src/main/java/br/com/okayamafilho/tpedrapapelcompose/MainActivity.kt
+++ app/src/main/java/br/com/okayamafilho/tpedrapapelcompose/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +24,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.okayamafilho.tpedrapapelcompose.ui.theme.TPedraPapelComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,25 +42,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier){
+fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Cabecalho("Escolha o App")
+        RoundImageButtonsRowEscolhaPreview()
+        EscolhaOpcao(text1 = "Escolha uma opcao")
         RoundImageButtonsRowPreview()
+
     }
 }
 
 @Composable
-fun RoundImageButtonsRow(imagePapel: Painter,
-                         imagePedra: Painter,
-                         imageTesoura: Painter,
-                         onClickImage1: () -> Unit,
-                         onClickImage2: () -> Unit,
-                         onClickImage3: () -> Unit
+fun RoundImageButtonsRow(
+    imagePapel: Painter,
+    imagePedra: Painter,
+    imageTesoura: Painter,
+    onClickImage1: () -> Unit,
+    onClickImage2: () -> Unit,
+    onClickImage3: () -> Unit
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp),
         horizontalArrangement = Arrangement.SpaceAround,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -66,7 +75,7 @@ fun RoundImageButtonsRow(imagePapel: Painter,
             painter = imagePapel,
             contentDescription = "Imagem Papel",
             modifier = Modifier
-                .size(64.dp)
+                .size(100.dp)
                 .clip(CircleShape)
                 .clickable { onClickImage1() }
         )
@@ -74,7 +83,7 @@ fun RoundImageButtonsRow(imagePapel: Painter,
             painter = imagePedra,
             contentDescription = "Imagem Pedra",
             modifier = Modifier
-                .size(64.dp)
+                .size(100.dp)
                 .clip(CircleShape)
                 .clickable { onClickImage2() }
         )
@@ -82,21 +91,97 @@ fun RoundImageButtonsRow(imagePapel: Painter,
             painter = imageTesoura,
             contentDescription = "Imagem Tesoura",
             modifier = Modifier
-                .size(64.dp)
-                .clip(CircleShape).clickable { onClickImage3() }
+                .size(100.dp)
+                .clip(CircleShape)
+                .clickable { onClickImage3() }
         )
     }
+
+    ThreeTextsRow("Papel", "Pedra", "Tesoura")
 }
 
 @Composable
 fun RoundImageButtonsRowPreview() {
     RoundImageButtonsRow(
-        imagePapel = painterResource(id = R.drawable.papel), // Substitua pelas suas imagens
+        imagePapel = painterResource(id = R.drawable.papel),
         imagePedra = painterResource(id = R.drawable.pedra),
         imageTesoura = painterResource(id = R.drawable.tesoura),
         onClickImage1 = { /* Ação da imagem 1 */ },
         onClickImage2 = { /* Ação da imagem 2 */ },
         onClickImage3 = { /* Ação da imagem 3 */ }
+    )
+}
+
+@Composable
+fun Cabecalho(text1: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = text1, fontSize = 18.sp)
+    }
+}
+
+@Composable
+fun EscolhaOpcao(text1: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = text1, fontSize = 18.sp)
+    }
+}
+
+@Composable
+fun ThreeTextsRow(text1: String, text2: String, text3: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, bottom = 4.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = text1)
+        Text(text = text2)
+        Text(text = text3)
+    }
+}
+
+
+@Composable
+fun RoundImageButtonsRowEscolha(
+    imagePapel: Painter,
+    onClickImage1: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 26.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = imagePapel,
+            contentDescription = "Imagem Papel",
+            modifier = Modifier
+                .size(140.dp)
+                .clip(CircleShape)
+                .clickable { onClickImage1() }
+        )
+    }
+}
+
+@Composable
+fun RoundImageButtonsRowEscolhaPreview() {
+    RoundImageButtonsRowEscolha(
+        imagePapel = painterResource(id = R.drawable.padrao),
+        onClickImage1 = { /* Ação da imagem 1 */ },
     )
 }
 
