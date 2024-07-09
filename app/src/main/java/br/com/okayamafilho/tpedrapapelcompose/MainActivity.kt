f4b1ced934c48c2270941801e043fa8f0937e698
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2024-07-09 11:10:31 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/okayamafilho/tpedrapapelcompose/MainActivity.kt
@@ -0,0 +1,41 @@
+package br.com.okayamafilho.tpedrapapelcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.okayamafilho.tpedrapapelcompose.ui.theme.TPedraPapelComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TPedraPapelComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TPedraPapelComposeTheme {
+        HomeScreen()
+    }
+}
